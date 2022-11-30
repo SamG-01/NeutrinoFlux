7bdb6c294aea41c65342b3831d4f377fe42e8c9c
Sam G <sam1glosser@gmail.com>
2022-11-30 18:23:11 -0500

    rename rho to d

--- earth_density.py
+++ earth_density.py
@@ -12,18 +12,18 @@ def r(z, theta):
     return r
 
 # Density Functions
-avg_rho_earth = 5.51/(C.centi)**3 # mean mass density of the earth, in g/m^3
+avg_d_earth = 5.51/(C.centi)**3 # mean mass density of the earth, in g/m^3
 
-def rho_earth_average(r):
+def d_earth_average(r):
     """Returns the average mass density of the earth."""
-    return avg_rho_earth
+    return avg_d_earth
 
-def rho_earth_full(r):
+def d_earth_full(r):
     """Returns the mass density at a point in the earth as a function of the radius. Taken from the Preliminary Earth Model."""
     y = r/R_E
     r /= C.kilo # convert to km
     
-    rho = np.piecewise(
+    d = np.piecewise(
         r,
         [
             0 <= r < 1221.5,
@@ -51,19 +51,19 @@ def rho_earth_full(r):
         ]
     ) * 1/(C.centi)**3 # g/m**3
     
-    return rho
+    return d
 
-def _rho_earth(z, theta):
+def _d_earth(z, theta):
     """Returns the nucleon number density as a function of penetration distance z and penetration angle theta."""
-    return rho_earth_full(r(z, theta))
+    return d_earth_full(r(z, theta))
 
 # Density Plotting
 if __name__ == "__main__":
     for n in np.linspace(0, 0.9, 9):
         X = x(theta := np.pi * n)
         Z = np.linspace(0, X)
-        rho = [_rho_earth(z, theta) for z in Z]
-        plt.plot(Z, rho, label=f"$\\theta = {n:.1f}\pi$")
+        d = [_d_earth(z, theta) for z in Z]
+        plt.plot(Z, d, label=f"$\\theta = {n:.1f}\pi$")
 
     plt.xlabel("Distance Penetrated (m)")
     plt.ylabel(r"Mass Density ($g/m^3$)")
@@ -73,8 +73,8 @@ if __name__ == "__main__":
 
 # Attenuation of the Earth
 def attenuation_integral(theta):
-    """Returns the integral of rho_earth described above."""
-    return quad( _rho_earth, 0, x(theta), args=[theta] )[0]
+    """Returns the integral of d_earth described above."""
+    return quad( _d_earth, 0, x(theta), args=[theta] )[0]
 
 angles = np.linspace(0, np.pi, 150)
 attenuation = [attenuation_integral(angle) for angle in angles]
@@ -84,12 +84,12 @@ if __name__ == "__main__":
     with open("Fits/attenuation_function", "wb") as f:
         pickle.dump(attenuation_function, f)
 
-    plt.plot(angles, [avg_rho_earth * x(angle) for angle in angles], label="Average", linestyle="dashed", color="green")
+    plt.plot(angles, [avg_d_earth * x(angle) for angle in angles], label="Average", linestyle="dashed", color="green")
     plt.plot(angles, attenuation_function(angles), label="Actual Fit")
     plt.plot(angles, attenuation, linestyle="none", marker=".", markersize=3, label="Actual Points")
 
     plt.xlabel("$\\theta$ (rad)")
-    plt.ylabel("$\\alpha(\\theta) := \int_0^{x(\\theta)}{\\rho(r(z, \\theta)) \\: d z}$ ($g/m^2$)")
+    plt.ylabel("$\\alpha(\\theta) := \int_0^{x(\\theta)}{d(r(z, \\theta)) \\: d z}$ ($g/m^2$)")
     plt.title("Attenuation Parameter")
     plt.legend()
     plt.show()
